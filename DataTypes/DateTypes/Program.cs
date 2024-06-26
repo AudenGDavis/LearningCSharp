efc40419cfffe6a29b56ec0910d778e144ea26e9
AudenGDavis <audengdavis@gmail.com>
2024-06-26 08:54:42 -0400

    June 26th Update

---
+++ DataTypes/DateTypes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            String myName = "auden";
+
+            // u = signed value (can go negative)
+            uint myAge = 17;
+            
+            // s = signed (can go negative)
+            sbyte negNum = -17;
+
+
+            double pi = 3.14159;
+            short myShort = 32550;
+
+            long myLong = 12345;
+
+            // ? indicated nullablity
+            int? myNull = null;
+
+        }
+    }
+}
---
+++ MyCalculator/MyCalculator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MyCalculator
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Please enter the first number to add:");
+
+            string? userInput = Console.ReadLine();
+
+            float firstValue;
+            while(!float.TryParse(userInput, out firstValue)){
+                System.Console.WriteLine("incorrect format, try again:");
+                userInput = Console.ReadLine();
+
+            }
+
+            System.Console.WriteLine("enter the second number to add:");
+
+            userInput = Console.ReadLine();
+
+            float secondValue;
+            while(!float.TryParse(userInput, out secondValue)){
+                System.Console.WriteLine("incorrect format, try again:");
+                userInput = Console.ReadLine();
+
+            }
+
+            System.Console.WriteLine($"{firstValue} + {secondValue} = {firstValue + secondValue}");
+
+
+
+        }
+    }
+}
