d57cec3dd14e7eb055862805d2524e8a51c29d52
AudenGDavis <audengdavis@gmail.com>
2024-07-09 08:46:51 -0400

    July 9th Update

---
+++ QuizApp/QuizApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace QuizApp
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("started");
+            Dictionary<String,int> questions = new Dictionary<string, int>();
+            questions.Add("2 + 2 = ",4);
+            questions.Add("1 + 5 = ",6);
+            questions.Add("1 * 3 = ",3);
+            questions.Add("5 + 2 * 4 = ",13);
+            questions.Add("6 / 3 = ",2);
+            int score = 0;
+
+            foreach(KeyValuePair<String, int> question in questions)
+            {
+                System.Console.Write(question.Key);
+                int answer;
+                while(!int.TryParse(Console.ReadLine(),out answer))
+                {
+                    System.Console.WriteLine("invalid answer, try again");
+                    System.Console.Write(question.Key);
+                }
+                if(answer == question.Value)
+                {
+                    System.Console.WriteLine("Correct!");
+                    score += 1;
+                }
+                else
+                {
+                    System.Console.WriteLine("incorrect");
+                }
+            }
+                System.Console.WriteLine($"Score: {score}");
+        }
+    }
+}
--- SwitchStatement/SwitchStatement/Program.cs
+++ SwitchStatement/SwitchStatement/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Security.Cryptography;
+
+namespace SwitchStatement
+{
+    class MainClass
+    {
+        static void Main(String[] args)
+        {
+            Console.Write("enter your favorite ice cream flavor: ");
+            String? response = Console.ReadLine();
+            
+            
+            switch (response)
+            {
+                case "vanilla":
+                    System.Console.WriteLine("that's valid");
+                    break;
+                
+                case "chocolate":
+                    System.Console.WriteLine("that's valid");
+                    break;
+
+                case "mint":
+                    System.Console.WriteLine("you have good taste");
+                    break;
+
+                case "pistachio":
+                    System.Console.WriteLine("you're crazy");
+                    break;
+
+                default:
+                    System.Console.WriteLine("you didn't pick chocolate, vanilla, or mint therefore you picked wrong");
+                    break;
+            }
+        }
+    }
+}
